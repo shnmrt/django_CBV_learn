cd8f08b910af62e05d551f333be231e42743c960
Murat Şahin <sahinmurat3@gmail.com>
2022-06-12 17:26:25 +0300

    TemplateView

--- school/classroom/views.py
+++ school/classroom/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = 'classroom/home.html'
+
+class ThankYouView(TemplateView):
+    template_name = 'classroom/thank_you.html'
